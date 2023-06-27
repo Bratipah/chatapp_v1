1f96b909bbef11474ebe79d0422a07ae2b9777ca
AndroidJunkie <bratipah@gmail.com>
2023-06-27 16:51:07 +0300

    update firebase sign in

--- src/auth/signIn.jsx
+++ src/auth/signIn.jsx
@@ -1,14 +1,12 @@
 // import React from 'react';
-import { signInWithPopup } from "firebase/auth";
-import { auth, provider } from '../firebase/fireBaseConfig'
- 
+import { useContext } from "react";
+import { SignInContext } from "../context/googleSignInContext";
+
 function SignIn() {
-    const signInWithGoogle = () => {
-        signInWithPopup(auth,provider)
-        };
+    const {handleClick} = useContext(SignInContext)
     return (
         <div>
-           <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"onClick={signInWithGoogle}>Sign In With Google</button>
+           <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"onClick={handleClick}>Sign In With Google</button>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,10 @@
 import Chat from '../components/Chat';
 import { auth } from '../firebase/fireBaseConfig';
 import { useAuthState } from 'react-firebase-hooks/auth'
-import SignIn from '../auth/SignIn';
+import { SignInProvider } from '../context/googleSignIn';
+import SignIn from '../auth/signIn';
 import '../App.css'
-// import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar";
 
 function Home() {
     const [user] = useAuthState(auth)
@@ -12,7 +13,15 @@ function Home() {
       <>
       <div className='py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6'>
             <h2>Chat Any Day any Time</h2>
-            {user ? <Chat /> : < SignIn/>}
+            {user 
+            ? <Chat /> 
+            : <SignInProvider>
+                    <Navbar/>
+                        <SignIn />
+        
+                    {/* <Footer /> */}
+        </SignInProvider>
+            }
         </div>
       </>
     );
