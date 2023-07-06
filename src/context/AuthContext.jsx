3b68d3e9b9b6ed3d497c740160b837bd07d55676
Bratipah (she/her) <57480729+Bratipah@users.noreply.github.com>
2023-07-06 16:19:11 +0300

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -11,13 +11,12 @@ import { auth } from '../firebase/fireBaseConfig';
 
 const AuthContext = createContext();
 
-<<<<<<< HEAD
 export const AuthContextProvider = ({ children }) => {
   // const [user, setUser] = useState({});
 =======
  const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
->>>>>>> 5327f8dc1ed0d9ffea15f311e1e81ce755dc5379
+// >>>>>>> 5327f8dc1ed0d9ffea15f311e1e81ce755dc5379
 
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
