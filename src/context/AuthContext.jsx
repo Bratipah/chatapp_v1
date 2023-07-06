677a70926ba7c3cf0e9af4565ea94f88c1e95200
Bratipah (she/her) <57480729+Bratipah@users.noreply.github.com>
2023-07-06 18:17:48 +0300

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -11,7 +11,7 @@ import { auth } from '../firebase/fireBaseConfig';
 
 const AuthContext = createContext();
 
-export const AuthContextProvider = ({ children }) => {
+const AuthContextProvider = ({ children }) => {
   // const [user, setUser] = useState({});
  const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
@@ -42,4 +42,5 @@ export const AuthContextProvider = ({ children }) => {
       {children}
     </AuthContext.Provider>
   );
+ };
  export {AuthContext,AuthContextProvider};
